cd87dba31aab921e64cb37610f9e753c1aabd986
Begmuhommet Jumayev <begmuhommet.jumayev@gmail.com>
2022-04-21 23:28:42 +0800

    Added parcel core

--- server.js
+++ server.js
@@ -3,17 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const render = res => {
-  return res.render(`${__dirname}/dist/index.html`);
-};
-
 app.use(express.static('./dist'));
-
-app.get('/login', (req, res) => render(res));
-app.get('/registration', (req, res) => render(res));
-app.get('/chat', (req, res) => render(res));
-app.get('/error', (req, res) => render(res));
-app.get('/*', (req, res) => render(res));
+app.get('/*', (req, res) => {
+  return res.render(`${__dirname}/dist/index.html`);
+});
 
 app.listen(PORT, () => {
   console.log(`App run on ${PORT}`);
